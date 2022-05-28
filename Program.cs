f95887a20f43b2a67cfea36426aee1f0db021a75
SilverDollarKitties <105564724+SilverDollarKitties@users.noreply.github.com>
2022-05-28 15:27:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Create new chrome browser instane
+var driver = new ChromeDriver();
+
+//navigate to wikipedia
+driver.Url = "https://wikipedia.org";
+
+System.Console.WriteLine("CURRENT TITLE: " + driver.Title);
+
+//Locate Search field by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+//Click On element
+searchField.Click();
+
+//FIll Qa And Press Enter
+searchField.SendKeys("QA" + Keys.Enter);
+
+System.Console.WriteLine("TITLE AFTER SEARCH: " + driver.Title);
+//Close browser
+driver.Quit();
